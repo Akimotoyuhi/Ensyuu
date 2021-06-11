2100c4c8e19ac4fbc2da70f5a3b379dd0f9ac934
AkimotoYuhi <chiisanaaki@gmail.com>
2021-06-11 21:53:43 +0900

    タイトルの難易度選択画面にて、各難易度の総セル数と地雷の数を表記して、プレイする前から各難易度にどういった違いがあるのかを分かりやすくした

--- Assets/MineSweeper/Scripts/SceneChanger.cs
+++ Assets/MineSweeper/Scripts/SceneChanger.cs
@@ -7,6 +7,9 @@ public class SceneChanger : MonoBehaviour
 {
     private GameObject _gamemode;
     private GameObject _msLevelSelect;
+    private GameObject _msEasyText;
+    private GameObject _msNormalText;
+    private GameObject _msHardText;
     private AudioSource _as;
     [SerializeField] private AudioClip _se;
     void Start()
@@ -14,6 +17,12 @@ public class SceneChanger : MonoBehaviour
         _gamemode = GameObject.Find("GameMode");
         _msLevelSelect = GameObject.Find("msLevelSelect");
         _msLevelSelect.SetActive(false);
+        _msEasyText = GameObject.Find("msEasyText");
+        _msEasyText.SetActive(false);
+        _msNormalText = GameObject.Find("msNormalText");
+        _msNormalText.SetActive(false);
+        _msHardText = GameObject.Find("msHardText");
+        _msHardText.SetActive(false);
         _as = GetComponent<AudioSource>();
     }
     public void EasyScene()
@@ -46,5 +55,32 @@ public class SceneChanger : MonoBehaviour
         _as.PlayOneShot(_se);
         _gamemode.SetActive(true);
         _msLevelSelect.SetActive(false);
+    }
+
+    public void EasyTextEnter()
+    {
+        _msEasyText.SetActive(true);
+    }
+    public void EasyTextExit()
+    {
+        _msEasyText.SetActive(false);
+    }
+
+    public void NormalTextEnter()
+    {
+        _msNormalText.SetActive(true);
+    }
+    public void NormalTextExit()
+    {
+        _msNormalText.SetActive(false);
+    }
+
+    public void HardTextEnter()
+    {
+        _msHardText.SetActive(true);
+    }
+    public void HardTextExit()
+    {
+        _msHardText.SetActive(false);
     }
 }
