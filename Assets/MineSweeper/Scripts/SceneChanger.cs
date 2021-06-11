6404dd9691d827c78c95b59d20bda883a4d10ace
AkimotoYuhi <chiisanaaki@gmail.com>
2021-06-11 20:37:10 +0900

    タイトル画面の見た目を整えた

--- Assets/MineSweeper/Scripts/SceneChanger.cs
+++ Assets/MineSweeper/Scripts/SceneChanger.cs
@@ -5,22 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    private GameObject _gamemode;
+    private GameObject _msLevelSelect;
+    private AudioSource _as;
+    [SerializeField] private AudioClip _se;
     void Start()
     {
-        
+        _gamemode = GameObject.Find("GameMode");
+        _msLevelSelect = GameObject.Find("msLevelSelect");
+        _msLevelSelect.SetActive(false);
+        _as = GetComponent<AudioSource>();
     }
     public void EasyScene()
     {
+        _as.PlayOneShot(_se);
         SceneManager.LoadScene("MinesweeperEasy");
     }
 
     public void NormalScene()
     {
+        _as.PlayOneShot(_se);
         SceneManager.LoadScene("MinesweeperNormal");
     }
 
     public void HardScene()
     {
+        _as.PlayOneShot(_se);
         SceneManager.LoadScene("MinesweeperHard");
+    }
+
+    public void MinesweeperButton()
+    {
+        _as.PlayOneShot(_se);
+        _gamemode.SetActive(false);
+        _msLevelSelect.SetActive(true);
+    }
+
+    public void MsBack()
+    {
+        _as.PlayOneShot(_se);
+        _gamemode.SetActive(true);
+        _msLevelSelect.SetActive(false);
     }
 }
